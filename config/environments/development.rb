ddf52b0c88614887cd338f1783c7862d6c4d7d22
Anthony Limere <anthonylimere@gmail.com>
2015-08-15 14:02:13 +0200

    basic devise views + navbar done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Devise configs - Edit :host when deploying on public domain
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+
 end
